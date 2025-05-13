e21a7ad5f89e5030e1de35d8b2e92ffa996a1d43
waseemikh <waseembusiness04@gmail.com>
2025-05-13 16:28:27 +0300

    some changes on style and installations

--- db/index.ts
+++
@@ -1,8 +0,0 @@
-// This file is maintained for compatibility with other parts of the application
-// We're actually using MongoDB for this project, not PostgreSQL
-
-// Create dummy exports to avoid breaking imports
-export const pool = null;
-export const db = { query: {}, insert: () => {}, select: () => {} };
-
-console.log("MongoDB is the primary database for this application. PostgreSQL is not being used.");
